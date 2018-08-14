cb37ccf0e248584d0e52a4f9e26896534f94f779
Samuel Marks <samuelmarks@gmail.com>
2018-08-15 00:08:48 +1000

    Wrap node in shell_env

--- offregister_app_push/ubuntu/app_builders.py
+++ offregister_app_push/ubuntu/app_builders.py
@@ -1,5 +1,6 @@
 from sys import modules
 
+from fabric.context_managers import shell_env
 from fabric.contrib.files import exists
 from fabric.operations import sudo, run
 
@@ -20,23 +21,25 @@ def build_node_app(kwargs, run_cmd):
     n_prefix = kwargs.get('N_PREFIX', run_cmd('echo $HOME/n', quiet=True))
     if not exists('{n_prefix}/bin'.format(n_prefix=n_prefix)):
         install_node0(node_version=kwargs.get('node_version'), use_sudo=False, node_sudo=False, N_PREFIX=n_prefix)
-    if not cmd_avail('npm'):
-        return '[Warn]: npm not installed; skipping'
-    npm_tmp = run_cmd('echo $HOME/.npm/_cacache/tmp', quiet=True)
-    install_global_npm_packages1(npm_global_packages=kwargs.get('npm_global_packages'), N_PREFIX=n_prefix,
-                                 use_sudo=False, node_sudo=False)
-    if run_cmd('npm i --unsafe-perm=true', warn_only=True).failed:
-        # sudo('chown -R {user} {npm_tmp}'.format(user=user, npm_tmp=npm_tmp))
-        run_cmd('chown -R {u} {d} {s}'.format(u=nonroot, d=kwargs['GIT_DIR'],
-                                              s='$(npm config get prefix)/{lib/node_modules,bin,share}', ))
-        sudo('npm i --unsafe-perm=true', user=nonroot)
-    if exists('typings.json'):
-        if cmd_avail('typings'):
-            run_cmd('rm -rf typings')
-            run_cmd('typings i')
-        else:
-            logger.warn('typings not installed; skipping')
-    if 'post_npm_step' in kwargs:
-        run_cmd(kwargs['post_npm_step'])
-    sudo('rm -rf {npm_tmp}'.format(npm_tmp=npm_tmp))
-    return '[node] app built'
+
+    with shell_env(PATH='$PATH:{n_prefix}/bin'.format(n_prefix=n_prefix)):
+        if not cmd_avail('npm'):
+            return '[Warn]: npm not installed; skipping'
+        npm_tmp = run_cmd('echo $HOME/.npm/_cacache/tmp', quiet=True)
+        install_global_npm_packages1(npm_global_packages=kwargs.get('npm_global_packages'), N_PREFIX=n_prefix,
+                                     use_sudo=False, node_sudo=False)
+        if run_cmd('npm i --unsafe-perm=true', warn_only=True).failed:
+            # sudo('chown -R {user} {npm_tmp}'.format(user=user, npm_tmp=npm_tmp))
+            run_cmd('chown -R {u} {d} {s}'.format(u=nonroot, d=kwargs['GIT_DIR'],
+                                                  s='$(npm config get prefix)/{lib/node_modules,bin,share}', ))
+            sudo('npm i --unsafe-perm=true', user=nonroot)
+        if exists('typings.json'):
+            if cmd_avail('typings'):
+                run_cmd('rm -rf typings')
+                run_cmd('typings i')
+            else:
+                logger.warn('typings not installed; skipping')
+        if 'post_npm_step' in kwargs:
+            run_cmd(kwargs['post_npm_step'])
+        sudo('rm -rf {npm_tmp}'.format(npm_tmp=npm_tmp))
+        return '[node] app built'
